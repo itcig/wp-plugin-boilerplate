9b2821b184ccddc64ebf41b20e44162faeb53d0f
Brad <bradley.ash@gmail.com>
2018-12-12 13:08:36 -0600

    Fixed upper pascal case substitutions

--- install.sh
+++ install.sh
@@ -10,7 +10,9 @@ read PLUGIN_NAME
 
 #echo "What is this class called (My_Plugin), followed by [ENTER]:"
 
+# Convert name to pascal case for classes
 PLUGIN_PASCAL="$(echo ${PLUGIN_NAME} | sed -e 's/ /_/')"
+PLUGIN_PASCAL_UPPER="$(echo ${PLUGIN_PASCAL} | tr '[:lower:]' '[:upper:]')"
 
 # Convert name to snake case for files
 PLUGIN_KEBAB="$(echo ${PLUGIN_NAME} | sed -e 's/ /\-/' | awk '{print tolower($0)}')"
@@ -32,8 +34,9 @@ done
 # Remove original files that remain
 rm -r plugin-name
 
-# Replace snake case instances
-find . -type f ! -name '*.sh' -exec sed -i '' -e 's/Plugin_Name/'${PLUGIN_PASCAL}'/g' {} +
+# Replace pascal case instances
+find . -type f ! -name '*.sh' -name 'README.md' -exec sed -i '' -e 's/Plugin_Name/'${PLUGIN_PASCAL}'/g' {} +
+find . -type f ! -name '*.sh' -name 'README.md' -exec sed -i '' -e 's/PLUGIN_NAME/'${PLUGIN_PASCAL_UPPER}'/g' {} +
 
 # Replace snake case instances
-find . -type f ! -name '*.sh' -exec sed -i '' -e 's/plugin-name/'${PLUGIN_KEBAB}'/g' {} +
+find . -type f ! -name '*.sh' -name 'README.md' -exec sed -i '' -e 's/plugin-name/'${PLUGIN_KEBAB}'/g' {} +
