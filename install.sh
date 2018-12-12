e9be9b175d88378409ac8a6657c85a6d7636ec6e
Brad <bradley.ash@gmail.com>
2018-12-12 15:41:11 -0600

    Changes to plugin name regex replacements

--- install.sh
+++ install.sh
@@ -10,6 +10,7 @@ read PLUGIN_NAME
 # Convert name to pascal case for classes
 PLUGIN_PASCAL="$(echo ${PLUGIN_NAME} | sed -e 's/ /_/')"
 PLUGIN_PASCAL_UPPER="$(echo ${PLUGIN_PASCAL} | tr '[:lower:]' '[:upper:]')"
+PLUGIN_PASCAL_LOWER="$(echo ${PLUGIN_PASCAL} | tr '[:upper:]' '[:lower:]')"
 
 # Convert name to snake case for files
 PLUGIN_KEBAB="$(echo ${PLUGIN_NAME} | sed -e 's/ /\-/' | awk '{print tolower($0)}')"
@@ -20,22 +21,21 @@ if [ -z ${PLUGIN_NAME+x} ] ; then
   exit 1
 fi
 
-# Copy all plugin files to new directory
-cp -r plugin-name "${PLUGIN_KEBAB}"
-
 # Fix non-ASCII characters and treat them as literals for sed commands below
 LANG=C
 LC_CTYPE=C
 
 # Replace pascal case instances in files
 find ./plugin-name -type f ! -path '*/\.*' -exec sed -i '' -e 's/Plugin_Name/'${PLUGIN_PASCAL}'/g' {} +
-find ./plugin-name -type f ! -path '*/\.*' -exec sed -i '' -e 's/PLUGIN_NAME/'${PLUGIN_PASCAL_UPPER}'/g' {} +
+find ./plugin-name -type f ! -path '*/\.*' -exec sed -i '' -e 's/PLUGIN_NAME_/'${PLUGIN_PASCAL_UPPER}_'/g' {} +
+find ./plugin-name -type f ! -path '*/\.*' -exec sed -i '' -e 's/_plugin_name/'${PLUGIN_PASCAL_LOWER}'/g' {} +
 
 # Replace snake case instances in files
 find ./plugin-name -type f ! -path '*/\.*' -exec sed -i '' -e 's/plugin-name/'${PLUGIN_KEBAB}'/g' {} +
 
 # Recursively rename files first
-find . -type f -name '*plugin-name*' | while read FILE ; do
+#find . -type f -name '*plugin-name*' | while read FILE ; do
+find ./plugin-name -type f | while read FILE ; do
     newfile="$(echo ${FILE} | sed -e 's/plugin-name/'${PLUGIN_KEBAB}'/g')" ;
     echo $FILE " --> " $newfile
     mkdir -p `dirname ${newfile}`
