467ad4379b6adb920d6bb6037f724fb21b94d010
Brad <bradley.ash@gmail.com>
2018-12-12 13:28:07 -0600

    Reorder install script items to ignore root folder

--- install.sh
+++ install.sh
@@ -23,6 +23,17 @@ if [ -z ${PLUGIN_NAME+x} ] ; then
   exit 1
 fi
 
+# Fix non-ASCII characters and treat them as literals for sed commands below
+LANG=C
+LC_CTYPE=C
+
+# Replace pascal case instances in files
+find ./plugin-name -type f ! -path '*/\.*' -exec sed -i '' -e 's/Plugin_Name/'${PLUGIN_PASCAL}'/g' {} +
+find ./plugin-name -type f ! -path '*/\.*' -exec sed -i '' -e 's/PLUGIN_NAME/'${PLUGIN_PASCAL_UPPER}'/g' {} +
+
+# Replace snake case instances in files
+find ./plugin-name -type f ! -path '*/\.*' -exec sed -i '' -e 's/plugin-name/'${PLUGIN_KEBAB}'/g' {} +
+
 # Recursively rename files first
 find . -type f -name '*plugin-name*' | while read FILE ; do
     newfile="$(echo ${FILE} | sed -e 's/plugin-name/'${PLUGIN_KEBAB}'/g')" ;
@@ -33,14 +44,3 @@ done
 
 # Remove original files that remain
 rm -r plugin-name
-
-# Fix non-ASCII characters and treat them as literals for sed commands below
-LANG=C
-LC_CTYPE=C
-
-# Replace pascal case instances
-find . -type f ! -name '*.sh' ! -name 'README.md' -exec sed -i '' -e 's/Plugin_Name/'${PLUGIN_PASCAL}'/g' {} +
-find . -type f ! -name '*.sh' ! -name 'README.md' -exec sed -i '' -e 's/PLUGIN_NAME/'${PLUGIN_PASCAL_UPPER}'/g' {} +
-
-# Replace snake case instances
-find . -type f ! -name '*.sh' ! -name 'README.md' -exec sed -i '' -e 's/plugin-name/'${PLUGIN_KEBAB}'/g' {} +
